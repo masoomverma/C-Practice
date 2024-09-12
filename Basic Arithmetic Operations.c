0bf1b98317eaafd3947c171d4a548d95c644212e
Masoomkumar Verma <masoomverma1@gmail.com>
2024-09-12 10:15:36 +0530

    Basic Arithmetic Operations.c

--- Basic Arithmetic Operations.c
+++ Basic Arithmetic Operations.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <conio.h>
 int main()
 {
     clrscr(); 
@@ -8,28 +7,21 @@ int main()
     scanf("%d",&a);
     printf("Enter the second number: ");
     scanf("%d",&b);
-    printf("What do you want to do.\n","1. Addition\n","2. Subctration\n","3. Multiplication\n","4. Division\n");
-    select = scanf("%d",&s);
-    if(select == 1)
+    printf("1. Addition\n","2. Subctration\n","3. Multiplication\n","4. Division\n","Enter your choise: \n");
+    scanf("%d",&s);
+    switch(s)
     {
+        case 1:
           add = a + b
           printf("The Addition is %d.",add);
-    }
-    if(select == 2)
-    {
+        case 2:
           sub = a + b
-          printf("The Subctraction is %d.",sub);
-    }
-    
-    if(select == 3)
-    {
+          printf("The Subctraction is %d.",sub);    
+        case 3:
           mul = a * b
           printf("The Multiplication is %d.",mul);
-    }
-    
-    if(select == 4)
-    {
-          div = a / b
+        case 4:
+            div = a / b
           printf("The Division is %d.",div);
     }
     getch();
