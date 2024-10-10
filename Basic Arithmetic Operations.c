f2003817d956bc007b61102a3d09eba59d617bbd
Masoomkumar Verma <masoomverma1@gmail.com>
2024-10-10 09:38:41 +0530

    Basic Arithmetic Operations.c

--- Basic Arithmetic Operations.c
+++ Basic Arithmetic Operations.c
@@ -1,41 +1,41 @@
 #include <stdio.h>
-int main()
-{
-    clrscr(); 
-    int s,a,b,add,sub,mul;
+
+int main() {
+    int s, a, b, add, sub, mul;
     float div;
     printf("Enter the first number: ");
-    scanf("%d",&a);
+    scanf("%d", &a);
     printf("Enter the second number: ");
-    scanf("%d",&b);
-    printf("1. Addition\n","2. Subctration\n","3. Multiplication\n","4. Division\n","Enter your choise: \n");
-    scanf("%d",&s);
-    switch(s)
-    {
+    scanf("%d", &b);
+
+    printf("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\nEnter your choice: ");
+    scanf("%d", &s);
+
+    switch(s) {
         case 1:
-          add = a + b
-          printf("The Addition is %d.\n",add);
+            add = a + b;
+            printf("The Addition is %d.\n", add);
+            break; 
         case 2:
-          sub = a + b
-          printf("The Subctraction is %d.\n",sub);    
+            sub = a - b;
+            printf("The Subtraction is %d.\n", sub);
+            break; 
         case 3:
-          mul = a * b
-          printf("The Multiplication is %d.\n",mul);
+            mul = a * b;
+            printf("The Multiplication is %d.\n", mul);
+            break; 
         case 4:
-            if(b != 0)
-            {
-                div = a / b
-                printf("The Division is %d.\n",div);
+            if (b != 0) {
+                div = (float)a / b; 
+                printf("The Division is %.2f.\n", div); 
+            } else {
+                printf("Error: Division by zero is not defined.\n");
             }
-            else
-            {
-                printf("Error, Division by zero is not DEFINE.\n");
-    
-            }
-            break;
-        default :
-            printf("Invalid Choise.\n");
-                
-            getch();
-    return 0;
+            break; 
+        default:
+            printf("Invalid Choice.\n");
+            break; 
+    }
+
+    return 0; 
 }
